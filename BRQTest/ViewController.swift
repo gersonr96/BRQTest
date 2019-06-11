6b86d517d2ee8f3d30b16434d0caf4b5874715b4
vieiragerson <vieira.r.gerson@gmail.com>
2019-06-10 21:45:18 -0300

    Initial Commit

---
+++ BRQTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BRQTest
+//
+//  Created by Gersinho on 10/06/19.
+//  Copyright © 2019 Gerson. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
