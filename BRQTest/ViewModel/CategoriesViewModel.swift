3a989221ef3044aa213938887c0395d9e2879010
Usuário Convidado <Guest@nbbv027064.local>
2019-06-26 10:27:07 -0300

    refatorando viewmodel

--- BRQTest/ViewModel/CategoriesViewModel.swift
+++ BRQTest/ViewModel/CategoriesViewModel.swift
@@ -7,7 +7,29 @@
 //
 
 import UIKit
-
+import ImageLoad
+struct CategoryViewModel{
+    let joke: Joke
+    private (set) var jokeTitle: String
+    private (set) var icon: UIImage
+    private (set) var jokeText: String
+    
+    init(currentJoke: Joke) {
+        self.joke = joke
+        loadJoke()
+    }
+    
+    private mutating func loadJoke(){
+        jokeTitle = setTitle(currentJoke: joke)
+        icon.load.request(with: joke.icon_url!)
+        jokeText = joke.value
+    }
+    
+    private func setTitle(currentJoke: Joke)-> String{
+         let genres = joke.categories!.joined(separator: ",")
+        return genres
+    }
+}
 class CategoriesViewModel {
     var apiManager: DevAPIManager?
     var view: CategoriesViewController?
